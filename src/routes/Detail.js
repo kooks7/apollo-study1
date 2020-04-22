46ec0c12b04a333d8c47d6b465a26c155eb929ea
kooks7 <busanminjae@naver.com>
2020-04-22 12:02:17 +0900

    Detail Image

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,33 @@
-export default () => 'Detail';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
+export default () => {
+  //1. url에서 query 받아와서
+  let { id } = useParams();
+  id = parseInt(id);
+  // 2. graphQL에 query,변수 보내고 데이터 받기
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id }
+  });
+  // 3. lodaing이면 로딩
+  if (loading) {
+    return 'loading';
+  }
+  // data있으면 데이터 가져오기
+  if (data && data.movie) {
+    return data.movie.title;
+  }
+};
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,67 @@
-export default () => 'Home';
+import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+import styled from 'styled-components';
+import Movie from '../components/Movie';
+
+// query 만들기
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600px;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+export default () => {
+  // useQuery를 사용해서 query 보내고 데이터 가졍괴
+  const { loading, data } = useQuery(GET_MOVIES);
+  return (
+    <Container>
+      <Header>
+        <Title>Apollo 2020</Title>
+        <Subtitle>I love GraphQL</Subtitle>
+      </Header>
+      {loading && <Loading>Loding ...</Loading>}
+      {/* 1 && 2 && 3 => 1 ,2 둘다 true면 3을 하라 */}
+      {!loading &&
+        data.movies &&
+        data.movies.map((m) => <Movie key={m.id} id={m.id} />)}
+    </Container>
+  );
+};
